9a2fec0cfa20fdfcc2926cc169c11940d0541732
Kerkion <kerkion1984@gmail.com>
2019-03-07 12:49:06 +0100

    Added PagesController,Connected ShoppingCartDB with entity framework using db set and db context,created a view for all pages that admin will see. Za Nikolu(sve je iskomentarisano pa mozes da pogledas).

--- ShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ ShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ShoppingCart.Models.Data;
+using ShoppingCart.Models.ViewModels.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,20 @@ namespace ShoppingCart.Areas.Admin.Controllers
         // GET: Admin/Pages
         public ActionResult Index()
         {
-            return View();
+            //Deklarisacemo listu PageVM
+            List<PageVM> pagesList;
+            //Korisitmo using iz razloga zato sto kad zavrsi sve ocisti za sobom(zatvori konekciju i sl.) moze i bez using ali ovako je sigurnije,pravimo novu istancu ShoppingCartDB
+            using (ShoppingCartDB db = new ShoppingCartDB())
+            {
+                //Inicijalizacija liste
+                //prvo pristupamo otvorenoj konekciji db,odatle biramo pages,prebacujemo sve to u array,zatim radimo order by po sortingu koji je(po defaultu je DESC(opadajuci),zatim odatle pristupamo PageVM u koji prosledjujemo PageDTO(jedan od razloga zasto smo pravili ustvari konstruktor u pageVM-u) i to sve prebacujemo u listu)
+                //Tako cemo dobiti narodski receno spisak svih stranica
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+            //Vratit View sa napravljenom listom
+            return View(pagesList);
+
+            //na kraju na desni klik bilo gde unutar ovog controlera isli smo na AddView(ostavili ime index,za template smo izabrali listu,i posle toga za model dodali PagesVM) i napravili novi view za PageController
         }
     }
 }
--- ShoppingCart/Models/Data/ShoppingCartDB.cs
+++ ShoppingCart/Models/Data/ShoppingCartDB.cs
@@ -4,10 +4,14 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
+/// <summary>
+/// Klasa namenjena za pristupanje bazi preko Entity Framework-a
+/// </summary>
 namespace ShoppingCart.Models.Data
 {
     public class ShoppingCartDB : DbContext
     {
-
+        //Setujemo pages
+        public DbSet<PageDTO> Pages { get; set; }
     }
 }
