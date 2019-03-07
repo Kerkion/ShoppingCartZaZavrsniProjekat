fe3b216a42db3c4eb22ed330e2d4a5450e8f3224
Kerkion <kerkion1984@gmail.com>
2019-03-07 12:03:13 +0100

    Made first table and connected it to an PageDTO,Also made an PageVM for that page PageDTO,Added PagesController. Za Nikolu Pogledaj Prethodni koji sam okaci i pogledaj koje su razlike u odnosu na novi commit,ulavnom je sve objasnjeno u komentarima.

---
+++ ShoppingCart/Models/Data/PageDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+
+/// <summary>
+/// Ovo je DTO class-a koja ce komunicirati sa tblPages u bazi podataka
+/// </summary>
+namespace ShoppingCart.Models.Data
+{
+    //Page DTO(DTO =  DATA TRANSFER OBJECT) nam sluzi da prebacimo sve iz baze podataka u property koje smo napravili.
+    //Data anotacije [Table("tblPages")] je koriscena zato sto se ime ove klase ne poklapa sa imenom table u bazi podataka pa entity framework ne bih znao na koju tableu pokusavamo da povezemo ovu klasu
+    [Table("tblPages")]
+    public class PageDTO
+    {
+        //Data Anotacija za primarni kljuc
+        //predstavlja ce Id nase stranice
+        [Key]
+        public int Id { get; set; }
+        //Predstaclja ce Title nase stranice
+        public string Title { get; set; }
+        //Predstavlja ce Slug nase stranice(Slug je  deo url-a koji indentifikuje stranicu,tako sto ce biti napisano da covek moze da razume
+        public string Slug { get; set; }
+        //Predstavljace Body nase stranice
+        public string Body { get; set; }
+        //Predstavlja ce sortiranje nasih stranica(jednostavnije receno ako budemo imali vise stranica mocicemo da ih sortiramo kako zelimo a sa ovim propertiem cemo pratiti gde se nalazi koji)
+        public int Sorting { get; set; }
+        //Predstavlja Side bar na nasoj stranici(mocicemo da biramo da li ce nasa stranica imati side bar ili ne)
+        public bool HasSidebar { get; set; }
+    }
+}
---
+++ ShoppingCart/Models/ViewModels/Pages/PageVM.cs
@@ -0,0 +1,46 @@
+﻿using ShoppingCart.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Ovo ce biti ViewModel za Admin Page koji je povezan class-om PageDTO
+/// </summary>
+namespace ShoppingCart.Models.ViewModels.Pages
+{
+    public class PageVM
+    {
+        //Osnovni Konstruktor
+        public PageVM()
+        {
+
+        }
+        //Pravimo konstruktor koji ce povuci iz PageDTO(Page (DTO je ustvari Data Transfer Object) i popuniti Property-e koji su isti ovde kao i tamo.
+        //Prilikom pravljenja ovog objekta bice pozvan PageDTO i on ce napuniti ovaj konstruktor 
+        public PageVM(PageDTO row)
+        {
+            Id = row.Id;
+            Title = row.Title;
+            Slug = row.Slug;
+            Body = row.Body;
+            Sorting = row.Sorting;
+            HasSidebar = row.HasSidebar;
+        }
+
+
+        public int Id { get; set; }
+        //Title ce biti obavezan za stranicu i mora da bude minimalno 3 karaktera duzine i maksimalno 50 karaktera(to znace ove anotacije)
+        [Required]
+        [StringLength(50,MinimumLength = 3)]
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        //Body ce biti obavezan za stranicu i mora da bude minimalno 3 karaktera duzine i maksimalno Koliko je Maksimalna vrednost inta to jest 2147483647 karaktera (to znace ove anotacije)
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 3)]
+        public string Body { get; set; }
+        public int Sorting { get; set; }
+        public bool HasSidebar { get; set; }
+    }
+}
