3a3a9fa990979f84204cc38e5d1b66f47c9a6922
Kerkion <kerkion1984@gmail.com>
2019-03-10 18:24:23 +0100

    Added a Ajax Category View where admin can add a Category,alo he can delete it.Added some styles ,gif for loading ajax etc...

--- ShoppingCart/Areas/Admin/Controllers/ShopController.cs
+++ ShoppingCart/Areas/Admin/Controllers/ShopController.cs
@@ -12,6 +12,7 @@ namespace ShoppingCart.Areas.Admin.Controllers
 {
     public class ShopController : Controller
     {
+
         // GET: Admin/Shop/Categories
         public ActionResult Categories()
         {
@@ -25,6 +26,81 @@ namespace ShoppingCart.Areas.Admin.Controllers
             };
             //vrati listu view-a
             return View(categoryVMList);
+        }
+
+        // Post: Admin/Shop/AddNewCategory
+        [HttpPost]
+        //catName dolazi iz Ajax
+        public string AddNewCategory(string catName)
+        {
+            //Deklarisati id
+            string id;
+
+            using (ShoppingCartDB db = new ShoppingCartDB())
+            {
+                //proveriti da li je category ime jedinstveno 
+                if (db.Categories.Any(x => x.Name == catName))
+                {
+                    return "titletaken";
+                }
+                //inicijalizovati DTO
+                CategoriesDTO dto = new CategoriesDTO();
+                //Dodati u DTO
+                dto.Name = catName;
+                dto.Slug = catName.Replace(" ", "-").ToLower();
+                //ista logika kao i za pageove kada se doda kategorija bice poslednja
+                dto.Sorting = 100;
+                //sacuvati DTO
+                db.Categories.Add(dto);
+                db.SaveChanges();
+                //uzeti ubaceni id
+                id = dto.Id.ToString();
+            }
+            //vratiti taj id
+            return id;
+        }
+
+        //POST: Admin/Shop/ReorderCategories
+        [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+            using (ShoppingCartDB db = new ShoppingCartDB())
+            {
+                // napraviti brojac
+                int br = 1;
+                //Deklarisanje CategoriesDTO
+                CategoriesDTO dto;
+                //Postaviti sorting za svaki page
+                foreach (var item in id)
+                {
+                    //pronadji Categories sa vrednoscu koju trenutno ima item u db
+                    dto = db.Categories.Find(item);
+                    //postavi soting da bude isti kao i br
+                    dto.Sorting = br;
+                    //sacuvaj promene u db
+                    db.SaveChanges();
+                    //inkrementuj brojac
+                    br++;
+                }
+
+            }
+        }
+
+        //Get: Admin/Shop/DeleteCategory/id
+        public ActionResult DeleteCategory(int id)
+        {
+
+            using (ShoppingCartDB db = new ShoppingCartDB())
+            {
+                //Pronaci Category sa id-om u bazi
+                CategoriesDTO dto = db.Categories.Find(id);
+                //Ukloniti taj Category
+                db.Categories.Remove(dto);
+                //Sacuvati promene u bazi
+                db.SaveChanges();
+            }
+            //Redirektovati na index
+            return RedirectToAction("Categories");
         }
     }
 }
