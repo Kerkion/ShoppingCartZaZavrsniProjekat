fcad719d48418667e065176bb4a88af2da4ee39c
Milos Kerkion <kerkion1984@gmail.com>
2019-03-27 12:13:28 +0100

    made new Iprincipal for users,also made changes to shared layout so if you are admin then it won't show check out,insted of it it iwill pe an link to admin dashboard,and also added an activ class to user profile so it shows on nav bar.

--- ShoppingCart/Global.asax.cs
+++ ShoppingCart/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ShoppingCart.Models.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +18,36 @@ namespace ShoppingCart
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_AuthenticateRequest()
+        {
+            //proveriti da li je korisnik logovan
+            if(User == null)
+            {
+                return;
+            }
+
+            //pronaci username
+            string username = Context.User.Identity.Name;
+
+            //deklarisati array rols
+            string[] roles = null;
+            using (ShoppingCartDB db = new ShoppingCartDB())
+            {
+                //napuniti array roles
+                UserDTO dto = db.Users.FirstOrDefault(x => x.Username == username);
+                //izabrati koji je naziv rola
+                roles = db.UserRoles.Where(x => x.UserId == dto.Id).Select(x => x.Role.Roles).ToArray();
+                
+            }
+            //izgraditi IPrincipal obj
+            IIdentity userIdentity = new GenericIdentity(username);
+
+            IPrincipal newUserObject = new GenericPrincipal(userIdentity, roles);
+
+            //Update-ovati Context.User
+            Context.User = newUserObject;
         }
     }
 }
